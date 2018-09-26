4d92c9f278054ac7c4a5a4c47093ca5b2f6c3984
Moonunitz <31192468+Moonunitz@users.noreply.github.com>
2018-09-26 15:45:57 +1200

    Death Script Removed
    Death Script is now part of health script.

--- Prototype 4/Assets/Scripts/Enemies/EnemyHealthScript.cs
+++ Prototype 4/Assets/Scripts/Enemies/EnemyHealthScript.cs
@@ -24,6 +24,16 @@ public class EnemyHealthScript : MonoBehaviour
         m_fHealth -= _Damage;
         if (0.0001f > m_fHealth)
         {
+            GameObject Temp = Resources.Load<GameObject>("X");
+            Vector3 vTemp = this.transform.position;
+            short sX = 0;
+            while (6 > sX)
+            {
+                GameObject Temp2 = Instantiate(Temp);
+                Temp2.transform.position = this.transform.position + new Vector3(Random.Range(-0.3f, 0.3f), -1.0f, Random.Range(-0.3f, 0.3f));
+                Temp2.transform.Rotate(new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
+                sX++;
+            }
             Destroy(gameObject);
         }
     }
