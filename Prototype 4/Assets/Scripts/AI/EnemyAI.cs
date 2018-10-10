482d37de7d8690d081cd5ae543837b80c9c13bc4
Cloe Thermidor <Cloe.The7663@mds.network>
2018-10-10 17:04:00 +1300

    null conditions

--- Prototype 4/Assets/Scripts/AI/EnemyAI.cs
+++ Prototype 4/Assets/Scripts/AI/EnemyAI.cs
@@ -37,7 +37,7 @@ public class EnemyAI : MonoBehaviour {
         m_bArraySet = false;
         m_fDelay = 0.0f;
         m_vForward = new Vector3(1.0f, 0.0f, 0.0f);
-        m_Player = GameObject.FindGameObjectsWithTag("Player")[0];
+        m_Player = GameObject.FindGameObjectWithTag("Player");
         m_rRandom = new System.Random();
         m_fDamage = 5.0f;
         m_fDamageDelay = 0.0f;
@@ -48,56 +48,62 @@ public class EnemyAI : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (0.0f > m_fDamageDelay)
+        if (null != m_Player)
         {
-            if (1.5f > Vector3.Distance(this.transform.position, m_Player.transform.position))
+            if (0.0f > m_fDamageDelay)
             {
-                m_Player.GetComponent<Player>().TakeDamage(m_fDamage);
-                m_fDamageDelay = 1.0f;
+                if (1.5f > Vector3.Distance(this.transform.position, m_Player.transform.position))
+                {
+                    m_Player.GetComponent<Player>().TakeDamage(m_fDamage);
+                    m_fDamageDelay = 1.0f;
+                }
             }
-        }
-        m_vTarget = m_Player.transform.position - this.transform.position;
-        m_fDistance = Vector3.Magnitude(m_vTarget);
-        if (20.0f > m_fDistance)
-        {
-            m_fSpeed = c_fMaxSpeed;
-
-            if (0.5f > m_fDistance)
+            m_vTarget = m_Player.transform.position - this.transform.position;
+            m_fDistance = Vector3.Magnitude(m_vTarget);
+            if (20.0f > m_fDistance)
             {
-                m_Player.GetComponent<Player>().TakeDamage(5.0f);
+                m_fSpeed = c_fMaxSpeed;
+
+                if (0.5f > m_fDistance)
+                {
+                    m_Player.GetComponent<Player>().TakeDamage(5.0f);
+                }
             }
-        }
-        else
-        {
-            m_fSpeed = c_fMaxSpeed;
-        }
-        m_vForward += ((Vector3.Normalize(m_vTarget - m_vForward) * c_fMaxSpeed) - m_vForward) * c_fForce;
-        Vector3 Temp = ((m_vForward * m_fSpeed) * Time.deltaTime);
+            else
+            {
+                m_fSpeed = c_fMaxSpeed;
+            }
+            m_vForward += ((Vector3.Normalize(m_vTarget - m_vForward) * c_fMaxSpeed) - m_vForward) * c_fForce;
+            Vector3 Temp = ((m_vForward * m_fSpeed) * Time.deltaTime);
 
-        if (gameObject.transform.position != m_vTarget)
-        {
-            Agent.SetDestination(m_vTarget);
-        }
-        else
-        {
-            Agent.isStopped = true;
-        }
-        
+            if (gameObject.transform.position != m_vTarget)
+            {
+                Agent.SetDestination(m_vTarget);
+            }
+            else
+            {
+                Agent.isStopped = true;
+            }
 
 
 
-         Quaternion targetRotation = Quaternion.LookRotation(m_Player.transform.position - transform.position);
 
-        //transform.rotation = targetRotation;
-        m_vTarget -= Temp;
+            Quaternion targetRotation = Quaternion.LookRotation(m_Player.transform.position - transform.position);
+
+            //transform.rotation = targetRotation;
+            m_vTarget -= Temp;
 
 
             m_fDelay += Time.deltaTime;
-         
-                m_Followers = GameObject.FindGameObjectsWithTag("Follower");
-                m_bArraySet = true;
-        m_fDamageDelay -= Time.deltaTime;
 
+            m_Followers = GameObject.FindGameObjectsWithTag("Follower");
+            m_bArraySet = true;
+            m_fDamageDelay -= Time.deltaTime;
+        }
+        else
+        {
+            m_Player = GameObject.FindGameObjectWithTag("Player");
+        }
     }
 
     void Steer(Vector3 _Direction, float _Force)
