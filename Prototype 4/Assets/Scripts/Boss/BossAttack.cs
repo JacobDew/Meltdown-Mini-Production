1c0131a46148820254c6ffbdbd1a35521bdb1ec7
JacobDew <jacob.dewse@gmail.com>
2018-10-15 20:03:28 +1300

    Setup of boss attacks

---
+++ Prototype 4/Assets/Scripts/Boss/BossAttack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAttack : MonoBehaviour {
+
+    //Create a timer to script everything nicely
+
+
+	// Use this for initialization
+	void Start () {
+
+        Invoke("Attack1", 2.0f /*seconds*/); //use this to enable attacks
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    void Attack1()
+    {
+
+
+
+    }
+
+    void Attack2()
+    {
+
+
+
+    }
+    //etc
+}
