1d31b0000bda828305d93a635d7889d8b77e6af1
Moonunitz <31192468+Moonunitz@users.noreply.github.com>
2018-10-16 17:44:56 +1300

    Revert "fkthscrp"
    This reverts commit 6b36a8bc1605824f15641a168da5396e8de7ef6c.

--- Prototype 4/Assets/Scripts/Boss/BossAttack.cs
+++ Prototype 4/Assets/Scripts/Boss/BossAttack.cs
@@ -2,38 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossAttack : MonoBehaviour
-{
-    private GameObject m_pPlayer;
-
-    private GameObject[] m_pAttacks;
-
-    private string[] m_sFunctions;
+public class BossAttack : MonoBehaviour {
 
     //Create a timer to script everything nicely
 
 
 	// Use this for initialization
-	void Start()
-    {
-        Invoke(m_sFunctions[Random.Range(0, m_sFunctions.Length)], 0.0f); //use this to enable attacks
+	void Start () {
+
+        Invoke("Attack1", 2.0f /*seconds*/); //use this to enable attacks
 	}
 	
 	// Update is called once per frame
-	void Update()
-    {
+	void Update () {
 		
 	}
 
-    private void LoadAssets()
-    {
-        m_pAttacks = new GameObject[] { Resources.Load<GameObject>("EffectCubeZ") };
-    }
+
 
     void Attack1()
     {
-        GameObject TempPtr = Instantiate(m_pAttacks[0]);
-        TempPtr.AddComponent<SpiralShot>().SetDirection(GameObject.FindGameObjectWithTag("Player").transform.position - this.transform.position);
+
 
 
     }
--- Prototype 4/Assets/Scripts/Player/Currency.cs
+++ Prototype 4/Assets/Scripts/Player/Currency.cs
@@ -9,7 +9,6 @@ public class Currency : MonoBehaviour
     private GameObject m_pPlayer;
 
     private int[] m_iPrice;
-    private bool[] m_bPurchased;
 
 
 	// Use this for initialization
@@ -18,61 +17,38 @@ public class Currency : MonoBehaviour
         m_pPlayer = GameObject.FindGameObjectWithTag("Player");
         m_pPerkMenu = GameObject.FindGameObjectWithTag("PerkMenu");
         m_pPerkMenu.SetActive(false);
-        m_bPurchased = new bool[] { false, false, false };
         LoadWares();
+        DontDestroyOnLoad(m_pPerkMenu);
 	}
 
     private void LoadWares()
     {
-        m_iPrice = new int[] { 2000, 3000, 5000 };
+        m_iPrice = new int[] { 1, 1, 1 };
     }
 
     public void OpenPerkMenu()
     {
-        Debug.Log("Pause");
         m_pPerkMenu.SetActive(true);
         Time.timeScale = 0.0f;
     }
 
     public void PerkOne()
     {
-        if (false == m_bPurchased[0])
-        {
-            Debug.Log("Perk1");
-            if (true == m_pPlayer.GetComponent<Player>().AddPerk(0, m_iPrice[0]))
-            {
-                m_bPurchased[0] = true;
-            }
-        }
+        m_pPlayer.GetComponent<Player>().AddPerk(0, m_iPrice[0]);
     }
 
     public void PerkTwo()
     {
-        if (false == m_bPurchased[1])
-        {
-            Debug.Log("Perk2");
-            if (true == m_pPlayer.GetComponent<Player>().AddPerk(1, m_iPrice[1]))
-            {
-                m_bPurchased[1] = true;
-            }
-        }
+        m_pPlayer.GetComponent<Player>().AddPerk(1, m_iPrice[1]);
     }
 
     public void PerkThree()
     {
-        if (false == m_bPurchased[2])
-        {
-            Debug.Log("Perk3");
-            if (true == m_pPlayer.GetComponent<Player>().AddPerk(2, m_iPrice[2]))
-            {
-                m_bPurchased[2] = true;
-            }
-        }
+        m_pPlayer.GetComponent<Player>().AddPerk(2, m_iPrice[2]);
     }
 
     public void Return()
     {
-        Debug.Log("Resume");
         m_pPerkMenu.SetActive(false);
         Time.timeScale = 1.0f;
     }
