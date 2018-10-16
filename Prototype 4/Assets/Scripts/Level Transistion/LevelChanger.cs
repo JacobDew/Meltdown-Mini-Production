a51a363c8f27478ca39f120b8e1efc5e7440c6a6
Moonunitz <31192468+Moonunitz@users.noreply.github.com>
2018-10-16 17:50:51 +1300

    Revert "Revert "Merge branch 'master' of https://github.com/JacobDew/Meltdown-Mini-Production""
    This reverts commit 3f6c9d170453e8612c1cb101bb81a7eecc6d6f4b.

---
+++ Prototype 4/Assets/Scripts/Level Transistion/LevelChanger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour {
+
+    private Animator animator;
+    private string m_PLevelName;
+
+	// Use this for initialization
+	void Start() {
+        
+        animator = gameObject.GetComponent<Animator>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+
+    public void FadeToLevel(string Levelname)
+    {
+        m_PLevelName = Levelname;
+        animator.SetTrigger("FadeOut");
+
+    }
+
+    public void OnFadeCmplete()
+    {
+        SceneManager.LoadScene(m_PLevelName);
+    }
+}
