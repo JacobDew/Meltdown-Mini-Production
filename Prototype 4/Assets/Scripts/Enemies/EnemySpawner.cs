af354e176e986eec5f7ff4948ecc0129bfc2312b
Moonunitz <31192468+Moonunitz@users.noreply.github.com>
2018-10-16 17:45:44 +1300

    Revert "hot fix"
    This reverts commit 139ea1eb19fdb222f0f3e7d7107fea2a94cb9f55.

--- Prototype 4/Assets/Scripts/Enemies/EnemySpawner.cs
+++ Prototype 4/Assets/Scripts/Enemies/EnemySpawner.cs
@@ -54,12 +54,6 @@ public class EnemySpawner : MonoBehaviour
     
     void Update()
     {
-        if (SceneManager.GetActiveScene().buildIndex == 0)
-        {
-            Destroy(this.gameObject);
-        }
-
-
         if (3 > m_iLevel)
         {
             m_f60 = 0.0167f / Time.deltaTime;
