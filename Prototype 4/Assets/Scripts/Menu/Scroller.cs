6f1d26ff4ce9c3c8c44f06dfba1e05d8e4b0c242
Janelle <nel-x@live.com>
2018-10-17 17:12:35 +1300

    Credits Complete

---
+++ Prototype 4/Assets/Scripts/Menu/Scroller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroller : MonoBehaviour {
+
+    private GameObject m_pFadecanvas;
+
+    // Use this for initialization
+    void Start () {
+        m_pFadecanvas = GameObject.Find("Fade Canvas");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+    }
+
+    public void Mainmneu()
+    {
+        m_pFadecanvas.GetComponent<LevelChanger>().FadeToLevel("Main");
+    }
+}
